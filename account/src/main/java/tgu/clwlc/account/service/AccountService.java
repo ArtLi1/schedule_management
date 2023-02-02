743f36f5527c3d272696ac23815193fcac36949d
Bro_L <3385609893@qq.com>
2023-02-02 23:34:08 +0800

    Revert "用户登陆，更改账户信息"
    This reverts commit 8ada05b0d97bb93c01e10f083d09db9b1aabaf3a.

--- account/src/main/java/tgu/clwlc/account/service/AccountService.java
+++
@@ -1,7 +0,0 @@
-package tgu.clwlc.account.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import tgu.clwlc.account.entity.User;
-
-public interface AccountService extends IService<User> {
-}
